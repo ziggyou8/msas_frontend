7a1d93ef5e9e9d3f4b218eab21aafb41791627bb
Magueye Gueye <magueye717@gmail.com>
2021-09-29 11:59:25 +0000

    fix cors bug

--- src/utilities/axio.config.js
+++ src/utilities/axio.config.js
@@ -4,6 +4,7 @@ axios.interceptors.request.use(
   function (config) {
     config.headers.Authorization = 'Bearer '+localStorage.getItem('token');
     config.headers['Content-Type'] = 'application/json';
+    config.headers['X-Requested-With'] = 'XMLHttpRequest';
  
   //config.baseURL = 'http://localhost:8000/api/v1/';
   config.baseURL = 'http://176.31.107.205/msas_backend/public/api/v1/';
